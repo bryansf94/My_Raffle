3310b69d816eb3e5a974ae813a4a725200e5f835
Brayan Henrique <90359278+bryansf94@users.noreply.github.com>
2024-07-02 04:30:09 -0300

    Subindo arquivo de conexão com o BD e o inicio da minha pasta index

---
+++ Public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="C:\Users\User\Desktop\DRIVE\Repositorios\My_Riffle\Public\img\icone.png" type="image/x-icon">
+<title>Home Page</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
---
+++ src/concet_db.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO('mysql:host:localhost;dbname=my_riffle', 'root', 'Pokebolavai97');
+?>
