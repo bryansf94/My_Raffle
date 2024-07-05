0ac79547a08f19fbfa158b1ac01c594233e94d8b
Brayan Henrique <90359278+bryansf94@users.noreply.github.com>
2024-07-05 05:15:42 -0300

    Adicionando: Arquivo Styles.css, Inclusão de favicon, imagem de logo incial.

--- Public/index.php
+++ Public/index.php
@@ -3,21 +3,29 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-<lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport"content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="icon" href="C:\Users\User\Desktop\DRIVE\Repositorios\My_Riffle\Public\img\icone.png" type="image/x-icon">
-<title>Home Page</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="icon" href="\img\icone.png" type="image/x-icon">
+  <link rel="stylesheet" href="css/Styles_index.css">
+<title>Home Page - My Riffle</title>
 </head>
 <body>
+  <main>
+    <section class="container-banner">
+      <div class="container-banner">
+        <img src="img/logo-banner.png" class="logo" alt="logo-banner">
+      </div>
+
+
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
-
+</main>
 </body>
 </html>
